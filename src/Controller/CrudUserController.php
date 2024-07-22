f18eec9b3a0d609f96be9ebba8433d0184b00c42
soumaiaa <soumaiaalouii@gmail.com>
2024-07-22 10:13:28 +0200

    LSF

--- src/Controller/CrudUserController.php
+++ src/Controller/CrudUserController.php
@@ -17,52 +17,6 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/crud/user')]
 class CrudUserController extends AbstractController
 {
-    // #[Route('/', name: 'app_crud_user_index', methods: ['GET'])]
-    // public function index(UserRepository $userRepository): Response
-    // {
-    //     return $this->render('crud_user/index.html.twig', [
-    //         'users' => $userRepository->findAll(),
-    //     ]);
-    // }
-
-    // #[Route('/new', name: 'app_crud_user_new', methods: ['GET', 'POST'])]
-    // public function new(Request $request, EntityManagerInterface $entityManager): Response
-    // {
-    //     $user = new User();
-    //     $form = $this->createForm(UserType::class, $user);
-    //     $form->handleRequest($request);
-
-    //     if ($form->isSubmitted() && $form->isValid()) {
-    //         $entityManager->persist($user);
-    //         $entityManager->flush();
-
-    //         return $this->redirectToRoute('app_crud_user_index', [], Response::HTTP_SEE_OTHER);
-    //     }
-
-    //     return $this->render('crud_user/new.html.twig', [
-    //         'user' => $user,
-    //         'form' => $form,
-    //     ]);
-    // }
-
-    // #[Route('/{id}', name: 'app_profile', methods: ['GET'])]
-    // public function show(User $user,Request $request, EntityManagerInterface $entityManager ): Response
-    // {
-    //     $form = $this->createForm(UserType::class, $user);
-    //     $form->handleRequest($request);
-
-    //     if ($form->isSubmitted() && $form->isValid()) {
-    //         $entityManager->flush();
-
-    //         return $this->redirectToRoute('app_profile', ['id' => $user->getId()], Response::HTTP_SEE_OTHER);
-    //     }
-    // //    dd($user);
-    //     return $this->render('crud_user/profile.html.twig', [
-    //         'user' => $user,
-    //         'form' => $form,
-    //     ]);
-    // }
-
     #[Route('/{id}/Profile', name: 'app_profile', methods: ['GET', 'POST'])]
     public function edit(Request $request, User $user, FileUploader  $fileUploader, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {
